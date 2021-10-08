434c1018283c8c59d53e9bab4b270f79d9af5fe1
NoorMdKhan123 <noormd6400@gmail.com>
2021-10-08 18:48:01 +0530

    Sorting with bubble sort

---
+++ BubbleSort.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BubbleSort
+{
+    class BubbleSort
+    {
+        private Node start;
+        public  void BubbleSortList()
+        { 
+        Node p, q, end;
+
+            for (end = null; end != start.link; end = p)
+            {
+                for (p = start; p.link != end; p = p.link)
+                {
+                    q = p.link;
+                    if (p.info > q.info)
+                    {
+                        int temp = p.info;
+                        p.info = q.info;
+                        q.info = temp;
+                    }
+
+                }
+            }
+        }
+        public void CreateList()
+        {
+            int i, n, data;
+            Console.WriteLine("Enter the no of nodes to be inserted");
+            n = Convert.ToInt32(Console.ReadLine());
+
+            if (n == 0)
+                return;
+            for(i=1;i<=n;i++)
+            {
+                Console.WriteLine("Enter the numbers to be inserted");
+                n = Convert.ToInt32(Console.ReadLine());
+               
+
+
+            }
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace BubbleSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] arr = { 78, 55, 45, 98, 13 };
+            BubbleSort var = new BubbleSort();
+            var.BubbleSortList();
+
+        }
+    
+    }
+}
